6953d4ef8025edb9bc5ab95a7ea481441c9a9fa6
Rutvik <r.panchal@sanedhealth.com>
2023-07-20 16:32:16 +0530

    [Add] Radio Widget

---
+++ lib/src/widgets/radio_widget.dart
@@ -0,0 +1,60 @@
+part of '../radio_form_field.dart';
+
+class RadioWidget extends StatelessWidget {
+  final ValueNotifier<String> radioValue;
+  final List<String> values, titles;
+  final Function? callback;
+  final FormFieldState<String>? fieldState;
+
+  const RadioWidget(
+    this.radioValue, {
+    super.key,
+    this.callback,
+    this.values = const ["yes", "no"],
+    this.titles = const ["Yes", "No"],
+    this.fieldState,
+  });
+
+  @override
+  Widget build(BuildContext context) => ValueListenableBuilder(
+        valueListenable: radioValue,
+        builder: (context, value, child) => Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              children: values
+                  .asMap()
+                  .map(
+                    (index, value) => MapEntry(
+                      index,
+                      RadioRowWidget(
+                        groupValue: radioValue.value,
+                        text: titles[index],
+                        value: value,
+                        onChanged: (val) {
+                          radioValue.value = val!;
+                          if (callback != null) callback!();
+                        },
+                        isError: fieldState?.hasError ?? false,
+                      ),
+                    ),
+                  )
+                  .values
+                  .toList(),
+            ),
+            if (fieldState != null && fieldState!.hasError)
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 20.0),
+                child: Text(fieldState!.errorText ?? '*',
+                    style: TextStyle(
+                        fontFamily: "OpenSans-Regular",
+                        color: ColorConstants.errorTextColor,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w300)),
+              ),
+          ],
+        ),
+      );
+}
+
+
