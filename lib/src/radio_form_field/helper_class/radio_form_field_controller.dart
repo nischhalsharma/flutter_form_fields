276b2c1fc8384c0e2a41ffdc61f8b5c9f974a3e9
Nischhal Sharma <to.nischhal@gmail.com>
2023-07-24 12:54:30 +0530

    [Fix] validation bug in RadioFormFieldController

--- lib/src/radio_form_field/helper_class/radio_form_field_controller.dart
+++ lib/src/radio_form_field/helper_class/radio_form_field_controller.dart
@@ -1,10 +1,10 @@
 part of '../radio_form_field.dart';
 
-class RadioFormFieldController extends ValueNotifier<String> {
-  RadioFormFieldController({String? value}) : super(value ?? '');
+class RadioFormFieldController extends ValueNotifier<dynamic> {
+  RadioFormFieldController({dynamic value}) : super(value);
   String get selectedValue => super.value;
   void clear() {
-    super.value = '';
+    super.value = null;
     notifyListeners();
   }
 }
